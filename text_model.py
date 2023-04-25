8e0b3d1016a11b142cbd0c0891ae2f7186dc67fc
Kirill <37981417+K0RTEK@users.noreply.github.com>
2023-04-26 00:28:19 +0300

    Update text_model.py

--- text_model.py
+++ text_model.py
@@ -1,54 +1,56 @@
-import pandas as pd
-import re
-import nltk
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import accuracy_score
-from sklearn.naive_bayes import MultinomialNB
-import joblib
+import pandas as pd  # импортирование библиотеки pandas и задание краткого имени pd
+import re  # импортирование библиотеки re (Regular Expression) для работы с регулярными выражениями
+import nltk  # импортирование библиотеки nltk (Natural Language Toolkit) для работы с естественным языком
+from sklearn.feature_extraction.text import \
+    CountVectorizer  # импортирование класса CountVectorizer для извлечения признаков из текстов
+from sklearn.model_selection import train_test_split, \
+    GridSearchCV  # импортирование функций для разбиения на обучающую и тестовую выборки, а также для подбора гиперпараметров с помощью кросс-валидации
+from sklearn.pipeline import Pipeline  # импортирование класса Pipeline для создания цепочек преобразований и моделей
+from sklearn.metrics import accuracy_score  # импортирование метрики accuracy_score для оценки качества классификации
+from sklearn.naive_bayes import \
+    MultinomialNB  # импортирование класса MultinomialNB для построения наивного байесовского классификатора
+import joblib  # импортирование модуля joblib для сохранения модели в файл
+
 # Загрузка данных
-data = pd.read_csv('train_data.csv')
+data = pd.read_csv(
+    'train_data.csv')  # чтение данных из csv-файла и создание DataFrame с помощью метода read_csv из pandas
+
 # Предобработка текстов
-data['text'] = data['text'].fillna('').apply(lambda x: re.sub(r'[^a-zA-Zа-яА-Я]', ' ', x))
+data['text'] = data['text'].fillna('').apply(lambda x: re.sub(r'[^a-zA-Zа-яА-Я]', ' ',
+                                                              x))  # заполнение пропущенных значений в столбце text пустой строкой, затем удаление всех символов, кроме букв русского и английского алфавитов
 
 # Разделение на признаки и целевую переменную
-X = data['text']
-y = data['tag']
+X = data['text']  # выбор столбца text в качестве признаков
+y = data['tag']  # выбор столбца tag в качестве целевой переменной
+
 # Разбиение на тренировочную и тестовую выборки
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
+                                                    random_state=42)  # разбиение данных на обучающую и тестовую выборки в соотношении 80/20
 
 # Создание пайплайна
-pipeline = Pipeline([
+pipeline = Pipeline([  # создание последовательности преобразований и модели в Pipeline
     ('vect', CountVectorizer(stop_words=nltk.corpus.stopwords.words('russian'))),
-    ('clf', MultinomialNB())
+    # извлечение признаков из текстов с помощью CountVectorizer, исключая стоп-слова русского языка из библиотеки NLTK
+    ('clf', MultinomialNB())  # модель классификации текстов на основе наивного Байеса с мультиномиальным распределением
 ])
 
 # Подбор гиперпараметров
-parameters = {
-    'vect__ngram_range': [(1, 1), (1, 2)],
+parameters = {  # задание сетки гиперпараметров для перебора
+    'vect__ngram_range': [(1, 1), (1, 2)],  # выбор диапазона n-грамм, которые будут извлекаться
     'vect__max_df': [0.5, 0.75, 1.0],
-    'clf__alpha': [0.1, 0.5, 1.0]
+    # выбор максимальной доли документов, в которых слово может появляться, чтобы оно было включено в словарь
+    'clf__alpha': [0.1, 0.5, 1.0]  # выбор коэффициента сглаживания для модели наивного Байеса
 }
-grid_search = GridSearchCV(pipeline, parameters, cv=5, n_jobs=-1)
-grid_search.fit(X_train, y_train)
+grid_search = GridSearchCV(pipeline, parameters, cv=5,
+                           n_jobs=-1)  # перебор гиперпараметров с помощью GridSearchCV и кросс-валидацией на 5 фолдах
+
+grid_search.fit(X_train, y_train)  # обучение модели на тренировочной выборке с использованием перебора гиперпараметров
 
 # Оценка качества модели на тестовой выборке
-y_pred = grid_search.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print('Accuracy on test set: {:.2f}'.format(accuracy))
+y_pred = grid_search.predict(X_test)  # получение предсказаний на тестовой выборке
+accuracy = accuracy_score(y_test, y_pred)  # вычисление точности модели на тестовой выборке
+print('Accuracy on test set: {:.2f}'.format(accuracy))  # вывод точности модели на тестовой выборке
 
-# # Оценка качества модели на отложенной выборке
-# data_holdout = pd.read_csv('holdout_data.csv')
-# data_holdout['text'] = data_holdout['text'].apply(lambda x: re.sub(r'[^a-zA-Zа-яА-Я]', ' ', x))
-# X_holdout = data_holdout['text']
-# y_holdout = data_holdout['tag']
-# y_pred_holdout = grid_search.predict(X_holdout)
-# accuracy_holdout = accuracy_score(y_holdout, y_pred_holdout)
-# print('Accuracy on holdout set: {:.2f}'.format(accuracy_holdout))
-
-
-joblib.dump(grid_search.best_estimator_, 'text_model.pkl')
-print('Best parameters: {}'.format(grid_search.best_params_))
-
+joblib.dump(grid_search.best_estimator_, 'text_model.pkl')  # сохранение лучшей модели в файл
+print('Best parameters: {}'.format(grid_search.best_params_))  # вывод лучших гиперпараметров, найденных при переборе
 
